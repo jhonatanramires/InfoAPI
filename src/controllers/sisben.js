11a55c5dde3ab809e3d10e7e3f2ab8f1337ead16
Jhonatan Ramirez <cid.jhonatanramirez@gmail.com>
2026-02-22 10:17:45 -0500

    sisben scrapping fix

--- src/controllers/sisben.js
+++ src/controllers/sisben.js
@@ -1,73 +1,69 @@
 import puppeteer from 'puppeteer';
 import { DocTypes } from '../libs/constans.js';
 
-const getSisben = async (document,type) => {
-  console.log("from getSisben: ", document,type)
+const getSisben = async (document, type) => {
+  console.log("from getSisben: ", document, type);
   const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
+    headless: false,
+    args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
+    executablePath: process.env.NODE_ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
   });
-  console.log("from getSisben: ",browser)
+
   const page = await browser.newPage();
-  var response = {
-    sisbenGrade: false,
-    sisben: false
-  }
-  await page.setViewport({
-    width: 717,
-    height: 598
-  });
-  await page.goto('https://reportes.sisben.gov.co/dnp_sisbenconsulta');
-  if (type !== undefined){
-    await page.click('#TipoID');
-    await page.select('#TipoID', type); 
-    await page.click('#documento'); 
-    await page.type('#documento', document); 
-    await page.click('#botonenvio'); 
-    await page.waitForSelector("body > div.container > main > div")
-    response = await page.evaluate(async () => {
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  await page.setViewport({ width: 717, height: 598 });
+
+  let response = { sisbenGrade: false, sisben: false };
+
+  try {
+    await page.goto('https://reportes.sisben.gov.co/dnp_sisbenconsulta', { waitUntil: 'networkidle0' });
+
+    const typesToTry = type !== undefined ? [type] : DocTypes;
+
+    for (const currentType of typesToTry) {
+      // Recargar la página para cada intento (para evitar estado sucio)
+      await page.goto('https://reportes.sisben.gov.co/dnp_sisbenconsulta', { waitUntil: 'networkidle0' });
+
+      // Seleccionar tipo de documento
+      await page.select('#TipoID', currentType);
+      // Escribir número de documento
+      await page.type('#documento', document);
+
+      // Enviar formulario y esperar navegación
       try {
-        return { sisbenGrade: await document.querySelector('body > div.container > main > div > div.card.border.border-0 > div:nth-child(3) > div > div.col-md-3.imagenpuntaje.border.border-0 > div:nth-child(3) > div > p').innerText, sisben: true}
-      } catch(err) {
-        console.log(err)
-        return { sisben: false, sisbenGrade: false }
+        await Promise.all([
+          page.waitForNavigation({ waitUntil: 'networkidle0' }),
+          page.click('#botonenvio')
+        ]);
+      } catch (navError) {
+        console.error('Error en navegación:', navError);
+        // Podrías analizar el contenido de la página para saber si es 400
+        const content = await page.content();
+        console.log(content.substring(0, 500)); // Ver mensaje de error
+        continue; // Siguiente tipo
       }
-    });
-  } else {
-    for (const types of DocTypes) {
-      await page.click('#TipoID');
-      await page.select('#TipoID', types); 
-      await page.click('#documento'); 
-      await page.type('#documento', document); 
-      await page.click('#botonenvio');
-      await page.waitForSelector("body > div.container > main > div")
-      response = await page.evaluate(async () => {
-        try {
-          return { sisbenGrade: await document.querySelector('body > div.container > main > div > div.card.border.border-0 > div:nth-child(3) > div > div.col-md-3.imagenpuntaje.border.border-0 > div:nth-child(3) > div > p').innerText, sisben: true}
-        } catch(err) {
-          console.log(err)
-          return { sisben: false, sisbenGrade: false }
-        }
-      });
-      if (response.sisbenGrade !== false){
-        break
+
+      // Verificar si la consulta fue exitosa (depende del DOM resultante)
+      try {
+        const grade = await page.$eval('body > div.container > main > div > div.card.border.border-0 > div:nth-child(3) > div > div.col-md-3.imagenpuntaje.border.border-0 > div:nth-child(3) > div > p', el => el.innerText);
+        response = { sisbenGrade: grade, sisben: true };
+        break; // Éxito, salir del bucle
+      } catch (evalError) {
+        // No se encontró el selector (probablemente no hay datos)
+        response = { sisben: false, sisbenGrade: false };
+        // Continuar con el siguiente tipo
       }
     }
+  } catch (error) {
+    console.error('Error general:', error);
+  } finally {
+    await browser.close();
   }
-  
-  await browser.close();
 
-  console.log("from getSisben: ", response)
-
-  return response
+  console.log("from getSisben: ", response);
+  return response;
 };
 
-export default getSisben
+export default getSisben;
---
+++ src/controllers/sisben2.js
@@ -0,0 +1,74 @@
+import puppeteer from 'puppeteer';
+import { DocTypes } from '../libs/constans.js';
+
+const getSisben = async (document,type) => {
+  console.log("from getSisben: ", document,type)
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  console.log("from getSisben: ",browser)
+  const page = await browser.newPage();
+  var response = {
+    sisbenGrade: false,
+    sisben: false
+  }
+  await page.setViewport({
+    width: 717,
+    height: 598
+  });
+  await page.goto('https://reportes.sisben.gov.co/dnp_sisbenconsulta');
+  if (type !== undefined){
+    await page.click('#TipoID');
+    await page.select('#TipoID', type); 
+    await page.click('#documento'); 
+    await page.type('#documento', document); 
+    await page.click('#botonenvio'); 
+    await page.waitForSelector("body > div.container > main > div")
+    response = await page.evaluate(async () => {
+      try {
+        return { sisbenGrade: await document.querySelector('body > div.container > main > div > div.card.border.border-0 > div:nth-child(3) > div > div.col-md-3.imagenpuntaje.border.border-0 > div:nth-child(3) > div > p').innerText, sisben: true}
+      } catch(err) {
+        console.log(err)
+        return { sisben: false, sisbenGrade: false }
+      }
+    });
+  } else {
+    for (const types of DocTypes) {
+      await page.click('#TipoID');
+      await page.select('#TipoID', types); 
+      await page.click('#documento'); 
+      await page.type('#documento', document); 
+      await page.click('#botonenvio');
+      await page.waitForSelector("body > div.container > main > div")
+      response = await page.evaluate(async () => {
+        try {
+          return { sisbenGrade: await document.querySelector('body > div.container > main > div > div.card.border.border-0 > div:nth-child(3) > div > div.col-md-3.imagenpuntaje.border.border-0 > div:nth-child(3) > div > p').innerText, sisben: true}
+        } catch(err) {
+          console.log(err)
+          return { sisben: false, sisbenGrade: false }
+        }
+      });
+      if (response.sisbenGrade !== false){
+        break
+      }
+    }
+  }
+  
+  await browser.close();
+
+  console.log("from getSisben: ", response)
+
+  return response
+};
+
+export default getSisben
