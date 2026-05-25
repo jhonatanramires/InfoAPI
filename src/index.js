5a4b2b0357f1c10e7de4828e0afa085a341bcd32
jhonatanramires <adm.jhonatanramirez@gmail.com>
2026-05-25 00:29:03 -0500

    use the new logger to create a message to say that the server is running and in what port and also moved the port hardcoded to the file constants instead of where it was

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 'use strict';
 import express from 'express';
 import indexRoutes from './routes/index.routes.js'
+import { logger } from './libs/logs.js';
+import { PORT } from './libs/constans.js';
 
 const app = express();
 
 app.use(indexRoutes)
 
-app.listen(3000);
+app.listen(PORT);
+
+logger.info("app running in port", PORT)
+logger.silly("lol")
